2cc6fc69aaf6d91f86e9fab0fc6f298216708af3
TranNhanThe <nhanthe133@gmail.com>
2024-02-26 16:37:59 +0700

    connect atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,13 +1,30 @@
+// const mongoose = require('mongoose');
+
+// async function connect() {
+//     try {
+//         await mongoose.connect('mongodb://127.0.0.1/f8_education_dev', 
+
+//         );
+//         console.log('Connect successfully!!!');
+//     } catch (error) {
+//         console.log('Connect Fail!');
+//     }
+// }
+
+// module.exports = { connect };
+
 const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/f8_education_dev', 
-       
-        );
-        console.log('Connect successfully!!!');
+        await mongoose.connect('mongodb+srv://nhanthe133:0977343132As@cluster0.uw1j0wh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Connected successfully to MongoDB Atlas!');
     } catch (error) {
-        console.log('Connect Fail!');
+        console.error('Failed to connect to MongoDB Atlas:', error);
     }
 }
 
